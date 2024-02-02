f203315935e49a978bab005d52fc95e91d1847ca
kirtiprya <kirtipriya1753@gmail.com>
2024-02-02 21:55:25 +0530

    again

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Card({title,image}) {
   return (
     <div className='backdrop-blur shadow-blue-500 shadow-lg rounded-xl
-    bg-green-200 p-20 m-10 bg-opacity-30'>
+    bg-green-200 p-5 bg-opacity-30'>
 
     <img src={image} alt="" />
    {title}
