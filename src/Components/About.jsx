7caf5d62dd81f1f286376e6d6ede230c29da1527
kalpanvarsha <you@example.com>
2024-02-06 13:36:10 +0530

    kk

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     
     <div className='relative'>
         <video  className='bg-cover max-h-100 max-w-100% p-2'src={video} autoPlay loop muted/>
-        {/* <p className='absolute left-1/2 top-1/2'>Hello</p> */}
+        <p className='absolute left-1/2 top-1/2'>Hello</p>
         
     </div>
   )
