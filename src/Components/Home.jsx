c08053b73e81ce7eda87dc1b6c87b5ac0189d16b
kalpanvarsha <you@example.com>
2024-02-09 21:26:14 +0530

    kk

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
       <h1 className='absolute left-10 top-20 font-bold size-19 text-5xl text-gray-700 '>Ride in Style, Drive with Pride!</h1>
         <h2 className='absolute left-10 top-28 m-5 font-bold size-25 text-4xl text-red-700'> Unlock your journey with our cars!</h2>
 
-       <div >
+       <div className='flex flex-row ' >
         <Card title={"hh"} image={L}/>
         <Card  title={"kk"}  image={B}/>
        
