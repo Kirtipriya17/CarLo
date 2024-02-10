e9351f7ed4aebe0d14e86a1023e9de0566eb34eb
kirtiprya <kirtipriya1753@gmail.com>
2024-02-10 18:27:32 +0530

    K

--- src/Pages/Cars/Mahindra Thar.jsx
+++ src/Pages/Cars/Mahindra Thar.jsx
@@ -3,12 +3,14 @@ import Props from '../../Components/Props'
 import H1 from '../../assets/1.jpg'
 import H2 from '../../assets/Brands/Ford.jpg'
 import H3 from '../../assets/Brands/Jeep.jpg'
+import { Container } from 'postcss'
 
 export default function MahindraThar() {
   return (
     <div>
       {/* <AddToCartButton/>
       <ShoppingCart/> */}
+       
         <Props pic1={H1} pic2={H2} pic3={H3} Car={"Mahindra Thar"} Price={"Rs 100000"} Engine={"1497 cc-2184 cc"} Power={"116.93-150.19bhp"} Torque={"300Nm - 320Nm"} Seat={"4"} Drive={"4WD / 2WD"} Mileage={"15.2 kmp"} Feature={"Digital Instrument Cluster Lane Change Indicator"}/>
 
     </div>
