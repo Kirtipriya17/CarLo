af3aca1f65b210e9a789aff5bc5d6ed10f4c42ff
kalpanvarsha <you@example.com>
2024-02-15 23:33:51 +0530

    pappu1

--- src/Pages/Brand/AstonMartin.jsx
+++ src/Pages/Brand/AstonMartin.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import CarCards from '../../Components/CarCards'
+import h1 from '../../assets/2.jpg'
 
 export default function AstonMartin() {
   return (
-    <div>AstonMartin</div>
+    <div>
+      <CarCards link={"/"} image={h1} brand={"AstonMartin"} car={"Aston Martin Vantage"} price={"₹ 2.95 crore"} fuelType={"Petrol"} mileage={"8.6 "} transmission={"Automatic(DCT)"}
+      />
+      <CarCards link={"/"} image={h1} brand={"AstonMartin"} car={"Aston Martin DB11"} price={"₹ 3.29 crore"} fuelType={"Petrol"} mileage={"8.9"} transmission={"Automatic(DCT)"}
+      />
+      <CarCards link={"/"} image={h1} brand={"AstonMartin"} car={"Aston Martin DB12"} price={"₹ 4.59 crore"} fuelType={"Petrol"} mileage={"8.9"} transmission={"Automatic(TC)"}
+      />
+      <CarCards link={"/"} image={h1} brand={"AstonMartin"} car={"Aston Martin DBX"} price={"₹ 3.82 - 4.63 crore"} fuelType={"Petrol"} mileage={"10.1"} transmission={"Automatic(DCT)"}
+      />
+    </div>
   )
 }
